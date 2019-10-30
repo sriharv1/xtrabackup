0dd735d5a688eac427a66a9bce5234f2851fde76
hv024036 <sriharsha.vallabaneni@cerner.com>
2019-10-30 16:26:31 +0530

    Full/Inc/Cleanup working

--- xtrabackup.py
+++ xtrabackup.py
@@ -1,97 +1,138 @@
-import os,datetime,shutil,pipes,time,subprocess
+#!/usr/bin/env python
 
-#docker run -it -v /Users/hv024036/Desktop/python:/scripts --name xtrabackup harsha_python:latest
+import os,datetime,shutil,pipes,time,subprocess,sys
 
-DB_HOST = 'cernzbxapp101' #ie. localhost
+filename = sys.argv[1]
+DATA_DIRECTORY = '/Users/hv024036/Desktop/python'
+BACKUP_PATH = '/Users/hv024036/Desktop/python/percona'
+days = datetime.date.today()
+filename = sys.argv[1]
+DB_HOST = 'centosvm01' #ie. localhost
 DB_USER = 'bkpuser'
 DB_USER_PASSWORD = 'bkpuser'
 db = 'zabbix'
-#DB_NAME = '/backup/dbnames.txt'
+cleanup = 2
 DATA_DIRECTORY = '/Users/hv024036/Desktop/python'
 BACKUP_PATH = '/Users/hv024036/Desktop/python/percona'
 cleanup = 2
 toaddr = 'sriharsha.vallabaneni@gmail.com'
-#wday = datetime.date.today().weekday()
-wday = 6
-print wday
 days = datetime.date.today()
-print days
 
-class Xtrabackup:
+
+
+class Xtrabackup():
 
      def fullbackup(self, days):
           date = days
-          TODAYBACKUPPATH = BACKUP_PATH + "/" + str(date)
+          TODAYBACKUPPATH = BACKUP_PATH + "/" + str(date) + "/full/"
           print TODAYBACKUPPATH
-          try:
-               # os.stat(TODAYBACKUPPATH)
-               print os.stat(TODAYBACKUPPATH)
-          except:
-               os.mkdir(TODAYBACKUPPATH)
-               print "Directory is created"
-
-          #dumpcmd = "xtrabackup --backup" + " --compress " + "--user " + DB_USER + " --password " + DB_USER_PASSWORD + " --target-dir=" + TODAYBACKUPPATH + " --datadir=" + DATA_DIRECTORY
-          dumpcmd = "ls -l"
-          #print dumpcmd
-          #os.system(dumpcmd)
-          retcode = os.system(dumpcmd)
-          print retcode
-          if ( retcode == 0 ):
-               ## To clean up the old backup's
-               print "I am executing"
-               #xf.cleanup(cleanup)
+          print os.path.isdir(TODAYBACKUPPATH)
+          if os.path.isdir(TODAYBACKUPPATH) == True:
+               print "Backup path is exsisting please remove the directory : " +"\"" + TODAYBACKUPPATH + "\""
+               exit()
           else:
-               print "Need to send email"
+               print "creating backup"
+               os.makedirs(TODAYBACKUPPATH)
+               dumpcmd = "xtrabackup --backup" + " --compress " + "--user " + DB_USER + " --password " + DB_USER_PASSWORD + " --target-dir=" + TODAYBACKUPPATH + " --datadir=" + DATA_DIRECTORY
+               # dumpcmd = "ls -l"
+               retcode = os.system(dumpcmd)
+               print retcode
+               if ( retcode == 0 ):
+                    ## To clean up the old backup's
+                    os.chdir(BACKUP_PATH)
+                    f = open("backupstatus.txt", "w+")
+                    f.write(TODAYBACKUPPATH)
+                    f.close()
+                    b = 0
+                    with open("inc.txt",'w') as f:
+                         f.write(str(b))              #writing a assuming it has been changed
+                    print "Executing clean up"
+                    xf.cleanup(cleanup)
+                    print "Send email once after cleanup"
+               else:
+                    print "send email that full backup is Failure"
 
-
-
-     def Incremental(self, wday, days):
+     def Incremental(self, days):
           date = days
-          wday = wday
           # daysago = datetime.datetime.now() - datetime.timedelta(days=wday)
           # base_path = daysago.date()
           # print daysago.date()
-          if (wday==0):
-               daysago = datetime.datetime.now() - datetime.timedelta(days=1)
-               inc_date = daysago.date()
-               TODAYBACKUPPATH = BACKUP_PATH + "/" + str(inc_date) + "/inc" + str(wday)
-               BASE_DIR = BACKUP_PATH + "/" + str(inc_date)
-               dumpcmd = "xtrabackup --backup" + " --compress " + "--user " + DB_USER + " --password " + DB_USER_PASSWORD + " --target-dir=" + TODAYBACKUPPATH + " --incremental-basedir=" + BASE_DIR
+          os.chdir(BACKUP_PATH)
+          if os.path.exists('backupstatus.txt') == True:
+               f = open("backupstatus.txt","r")
+               BASE_DIR = f.read()
                print BASE_DIR
-               print dumpcmd
-          else:
-               daysago = datetime.datetime.now() - datetime.timedelta(days=wday + 1)
-               inc_date = daysago.date()
-               print inc_date
-               TODAYBACKUPPATH = BACKUP_PATH + "/" + str(inc_date) + "/inc" + str(wday)
-               BASE_DIR = BACKUP_PATH + "/" + str(inc_date) + "/inc" + str(wday-1)
-               dumpcmd = "xtrabackup --backup" + " --compress " + "--user " + DB_USER + " --password " + DB_USER_PASSWORD + " --target-dir=" + TODAYBACKUPPATH + " --incremental-basedir=" + BASE_DIR
-               print dumpcmd
-               print TODAYBACKUPPATH
-               print BASE_DIR
+               BASE_BACKUP = '/'.join(BASE_DIR.split('/')[:-2])
+               print BASE_BACKUP
+               # x = BASE_DIR.split('/')[:-1]
+               # y = x[-1]
+               # print x
+               with open("inc.txt",'r') as f: #open a file in the same folder
+                   a = f.readlines()            #read from file to variable a
+               #use the data read
+               b = int(a[0])                    #get integer at first position
+               if b == 0:
+                    TODAYBACKUPPATH = BASE_BACKUP + "/inc" + str(b) + "/"
+                    print TODAYBACKUPPATH
+                    os.makedirs(TODAYBACKUPPATH)
+                    dumpcmd = "xtrabackup --backup" + " --compress " + "--user " + DB_USER + " --password " + DB_USER_PASSWORD + " --target-dir=" + TODAYBACKUPPATH + " --incremental-basedir=" + BASE_DIR
+                    dumpcmd = "ls -l"
+                    print "I am INC0 backup"
+                    retcode = os.system(dumpcmd)
+                    if ( retcode == 0 ):
+                         ## To clean up the old backup's
+                         os.chdir(BACKUP_PATH)
+                         b = b+1                          #increment
+                         with open("inc.txt",'w') as f:
+                              f.write(str(b))              #writing a assuming it has been changed
+                    else:
+                         print "send email that full backup is Failure"
+               else:
+                    BASE_DIR = BASE_BACKUP + "/inc" + str(b-1) + "/"
+                    TODAYBACKUPPATH = BASE_BACKUP + "/inc" + str(b) + "/"
+                    print TODAYBACKUPPATH
+                    os.makedirs(TODAYBACKUPPATH)
+                    dumpcmd = "xtrabackup --backup" + " --compress " + "--user " + DB_USER + " --password " + DB_USER_PASSWORD + " --target-dir=" + TODAYBACKUPPATH + " --incremental-basedir=" + BASE_DIR
+                    dumpcmd = "ls -l"
+                    print BASE_DIR
+                    print "I am INC" +str(b-1) + " backup"
+                    retcode = os.system(dumpcmd)
+                    if ( retcode == 0 ):
+                         ## To clean up the old backup's
+                         os.chdir(BACKUP_PATH)
+                         b = b+1                          #increment
+                         with open("inc.txt",'w') as f:
+                              f.write(str(b))              #writing a assuming it has been changed
+                    else:
+                         print "send email that full backup is Failure"
+
+     def restore(self,restore):
+          restday = restore
+          print restday
 
      def cleanup(self, cleanup):
           cleanup = cleanup
           print cleanup
           now = time.time()
           os.chdir(BACKUP_PATH)
+          print BACKUP_PATH     ### Need to comment
           try:
                for folder, directory, files in os.walk(BACKUP_PATH):
                     for i in directory:
                          print i + " is directory"
                          if (os.path.isdir(i)):
                               if (os.path.getmtime(i)) < now - cleanup * 86400:
-                                   #print(time.ctime(os.path.getmtime(i)), end= ' ')
-                                   print(time.ctime(os.path.getmtime(i)))
-                                   print(i)
+                                   print(time.ctime(os.path.getmtime(i)), )
+                                   #print(time.ctime(os.path.getmtime(i)))
+                                   print(i) ### Need to comment
                               else:
                                    print "No old files to delete"
                               #shutil.rmtree(i)
                     for j in files:
                          if (os.path.isfile(j)):
                               if (os.path.getmtime(j)) < now - cleanup * 86400:
-                                   #print(time.ctime(os.path.getmtime(j)), end = ' ')
-                                   print(time.ctime(os.path.getmtime(j)))
+                                   print(time.ctime(os.path.getmtime(j)), )
+                                   #print(time.ctime(os.path.getmtime(j)))
                                    print(j)
                                    #os.remove(j)
           except OSError as e:
@@ -99,31 +140,24 @@ class Xtrabackup:
 
 
 
-if (wday == 6):
+
+if filename == 'full':
      xf = Xtrabackup()
      xf.fullbackup(days)
-else:
+elif filename == 'Inc':
      xf = Xtrabackup()
-     xf.Incremental(wday, days)
-
-
-
-
-
-# xtrabackup --backup --user=bkpuser --password=bkpuser --target-dir=/var/lib/backups/percona --datadir=/var/lib/mysql/
-
-
-
-
-
-
-
-
-
-# class xtrabackup:
-#
-#      def __init__(self,full,inc):
-#           self.full = full
-#           self.inc = inc
-#
-#      def __init__
+     xf.Incremental(days)
+elif filename == 'cleanup':
+     xf = Xtrabackup()
+     xf.cleanup()
+elif filename == 'restore':
+     userIn = raw_input("Type Date dd/mm/yy: ")
+     restdays = datetime.datetime.strptime(userIn, "%d/%m/%y")
+     print restdays.date()
+     xf = Xtrabackup()
+     xf.restore(restdays)
+elif filename == "-h":
+     print "Usage of xtrabackup"
+     print "mysqlbackup <full> for full backup"
+     print "mysqlbackup <Inc> for Incremental backup, Make sure to have full backup before triggering for incremental backup"
+     print "mysqlbackup <restore> for preparing the backup for the restore. Please have the date"
